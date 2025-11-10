c6546b7672af02599da56297e3f6c8b161131f05
Emmanuel <eotoo218@gmail.com>
2025-11-10 22:12:24 +0000

    Refactor HTTP server implementation; remove `/hello` endpoint and `HelloHandler`, rename `Server.java` to `HttpServerApp.java`, and replace `HttpServer` with `ServerSocket`. Add updated IntelliJ IDEA configuration files.

---
+++ src/main/java/dev/emmanuelotoo/HttpServerApp.java
@@ -0,0 +1,35 @@
+package dev.emmanuelotoo;
+
+import java.net.ServerSocket;
+import java.nio.charset.StandardCharsets;
+
+public class HttpServerApp {
+    public static void main(String[] args) throws Exception  {
+        var serverSocket = new ServerSocket(8080);
+
+        while (true) {
+            var connection = serverSocket.accept();
+
+            try (var os = connection.getOutputStream()) {
+                var json = """
+                        {
+                            "id": 1
+                        
+                        }
+                        
+                        """;
+
+                var response = """
+                        HTTP/1.1 200 OK
+                        Content-Type: application/json
+                        Content-Length: %d
+                        
+                        %s
+                        """.formatted(json.getBytes(StandardCharsets.UTF_8).length, json);
+
+                os.write(response.getBytes(StandardCharsets.UTF_8));
+            }
+        }
+
+    }
+}
